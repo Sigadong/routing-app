ff406f51be3cda971f960a7954eba4967d277d29
Sigadong <irwanto.regar30@gmail.com>
2022-04-05 12:13:31 +0700

    lazy load & code splitting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route, NavLink, Redirect, Prompt } from 'react-router-dom';
-import Home from './pages/Home';
 import About from './pages/About';
 import Category from './pages/Category';
 import Post from './pages/Post';
@@ -9,51 +8,55 @@ import Login from './pages/Login';
 import GuardRoute from './components/GuardRoute';
 
 import './App.css';
+const Home = React.lazy(() => import('./pages/Home'));
 
 
 function App() {
   const [isLogin, setLogin] = React.useState(false);
 
   return (
-    <div div className="App" >
-      <ul className="menu">
-        <li><NavLink to="/" activeClass="active" exact>Home</NavLink></li>
-        <li><NavLink to="/about" activeClass="active">About</NavLink></li>
-        <li><NavLink to="/member" activeClass="active">Member</NavLink></li>
-        <li><NavLink to="/category" activeClass="active">Category</NavLink></li>
-        <li>
-          {isLogin ?
-            <NavLink to="/logout" onClick={(e) => {
-              e.preventDefault()
-              setLogin(false)
-            }}>Logout</NavLink> :
-            <NavLink to="/login" activeClass="active">Login</NavLink>
-          }
-        </li>
-      </ul>
-      <div className="main">
-        <Switch>
-          <Route path="/about">
-            <About />
-            <Prompt message="Are You sure want to leave?" />
-          </Route>
-          <GuardRoute path="/member" isLogin={isLogin}>
-            <Member />
-          </GuardRoute>
-          <Route path="/login">
-            <Login setLogin={setLogin} />
-          </Route>
-          <Route path="/category" component={Category}>
-          </Route>
-          <Route path="/post/:id">
-            <Post />
-          </Route>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </div >
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <div div className="App" >
+        <ul className="menu">
+          <li><NavLink to="/" activeClass="active" exact>Home</NavLink></li>
+          <li><NavLink to="/about" activeClass="active">About</NavLink></li>
+          <li><NavLink to="/member" activeClass="active">Member</NavLink></li>
+          <li><NavLink to="/category" activeClass="active">Category</NavLink></li>
+          <li>
+            {isLogin ?
+              <NavLink to="/logout" onClick={(e) => {
+                e.preventDefault()
+                setLogin(false)
+              }}>Logout</NavLink> :
+              <NavLink to="/login" activeClass="active">Login</NavLink>
+            }
+          </li>
+        </ul>
+        <div className="main">
+          <Switch>
+            <Route path="/about">
+              <About />
+              <Prompt message="Are You sure want to leave?" />
+            </Route>
+            <GuardRoute path="/member" isLogin={isLogin}>
+              <Member />
+            </GuardRoute>
+            <Route path="/login">
+              <Login setLogin={setLogin} />
+            </Route>
+            <Route path="/category">
+              <Category />
+            </Route>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div >
+    </React.Suspense>
   );
 }
 
